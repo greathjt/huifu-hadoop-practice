54b23160a7d2f49d4eaa51552dbd659d6f46c61d
alex <greathjt@163.com>
2016-12-19 12:34:15 +0800

    dev1:distributed cache read successfully

--- src/main/java/com/huifu/hadoop/hdfs/ReplicatedJoin.java
+++ src/main/java/com/huifu/hadoop/hdfs/ReplicatedJoin.java
@@ -1,5 +1,6 @@
 package com.huifu.hadoop.hdfs;
 
+
 import com.huifu.hadoop.hdfs.model.User;
 import com.huifu.hadoop.hdfs.model.UserLog;
 
@@ -14,13 +15,11 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.StringUtils;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,7 +27,10 @@ import java.util.Map;
  * Created by chao.hu on 2016/12/16.
  */
 public class ReplicatedJoin extends Configured implements Tool {
-
+    /**
+     * Execution: hadoop jar ./huifu-hadoop-practice-1.0.0-SNAPSHOT-shaded.jar
+     * com.huifu.hadoop.hdfs.ReplicatedJoin join/user.txt join/logs.txt output
+     */
     public static void main(String[] args) throws Exception {
         int res = ToolRunner.run(new Configuration(), new ReplicatedJoin(), args);
         System.out.println(res);
@@ -41,32 +43,32 @@ public class ReplicatedJoin extends Configured implements Tool {
         Path outputPath = new Path(args[2]);
 
         Configuration conf = super.getConf();
+        //先添加文件，再将conf传给job，否则mapper无法找到文件
+        DistributedCache.addCacheFile(usersPath.toUri(), conf);
         Job job = Job.getInstance(conf);
         job.setJarByClass(ReplicatedJoin.class);
-        DistributedCache.addCacheFile(usersPath.toUri(), conf);
-        //config.set("mapred.cache.files", usersPath.toUri().toString());
-        //System.out.println("<<<<<<<<<<<<<<<<<<<<<"+usersPath.toUri().toString());
         job.setMapperClass(JoinMap.class);
+        //否则：job.getConfiguration()，参见上一个注解
+        //DistributedCache.addCacheFile(usersPath.toUri(), job.getConfiguration());
         job.getConfiguration().set(JoinMap.DISTCACHE_FILENAME_CONFIG, usersPath.getName());
         FileInputFormat.addInputPath(job, logsPath);
         FileOutputFormat.setOutputPath(job, outputPath);
         job.setNumReduceTasks(0);
-
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
     public static class JoinMap extends Mapper<LongWritable, Text, Text, Text> {
         public static final String DISTCACHE_FILENAME_CONFIG = "replicatedjoin.distcache.filename";
-        private Map<String, User> users = new HashMap<String, User>();
+        private Map<String, User> users = new HashMap();
 
         @Override
         protected void setup(Context context) throws IOException, InterruptedException {
-            URI[] uris = DistributedCache.getCacheArchives(context.getConfiguration());
+            Path[] paths = DistributedCache.getLocalCacheFiles(context.getConfiguration());
             final String cacheFileName = context.getConfiguration().get(DISTCACHE_FILENAME_CONFIG);
             boolean found = false;
-            if (uris != null) {
-                for (URI uri : uris) {
-                    File file = new File(uri.getPath());
+            if (paths != null) {
+                for (Path path : paths) {
+                    File file = new File(path.getName());
                     if (cacheFileName.equals(file.getName())) {
                         loadCache(file);
                         found = true;
